214b1d2b52aef601c07dd174a01a8f892c6f3c34
Oumaim Meddah <oumaimameddah.98@gmail.com>
2023-06-03 05:15:00 +0200

    Add search input

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,8 +1,9 @@
 import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
+import { FilterMatchMode } from 'primereact/api';
 
 import "../styles/list-employees.style.scss"
 import {useSelector} from "react-redux";
@@ -10,7 +11,8 @@ import {selectEmployeeList} from "../hooks/use.utilitises";
 import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
 import 'primereact/resources/primereact.css';                       // core css
 import 'primeicons/primeicons.css';                                 // icons
-import 'primeflex/primeflex.css';                                   // css utility
+import 'primeflex/primeflex.css';
+import {InputText} from "primereact/inputtext";                                   // css utility
 
 
 const EmployeeList = () => {
@@ -34,12 +36,53 @@ const EmployeeList = () => {
         {name: "Zip Code", id: "zipCode", sortable: true},
     ];
 
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        firstName: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        lastName: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        startDate: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        department: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        dateOfBirth: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        city: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        state: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        zipCode: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        street: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
+    });
+
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+    const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+        setGlobalFilterValue(value);
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-end">
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                </span>
+            </div>
+        );
+    };
+
+    const header = renderHeader();
+
     return (
         <>
             <main className="list-employees-container card">
                 <div className="card">
                     <DataTable value={employees} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]}
-                               tableStyle={{minWidth: '50rem'}}>
+                               tableStyle={{minWidth: '50rem'}}
+                               header={header}
+                               filters={filters} filterDisplay="row"
+                               globalFilterFields={['firstName', 'lastName', 'startDate', 'department', 'dateOfBirth', 'street', 'city', 'state', 'zipCode']} emptyMessage="No employes found."
+                    >
                         {
                             columnsConfiguration
                                 .map((col, i) => (
